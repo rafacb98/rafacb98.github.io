45bf6b9872b868bb911c14bb3f816a27edbfe262
DESKTOP-KSC88M9\Rafa <rafaelcarrillobonilla@gmail.com>
2021-07-23 11:16:40 +0200

    Añade proyecto del buscaminas minificado a la sección de proyectos

---
+++ proyectos/buscaminas/precache-manifest.8ce755d4f2978365be4f5ebcf9cea921.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ad483e581989cd31c148952f513ee6",
+    "url": "rafacb98.github.io/proyectos/index.html"
+  },
+  {
+    "revision": "56bb782b27fe096d656c",
+    "url": "rafacb98.github.io/proyectos/static/css/2.2db45b02.chunk.css"
+  },
+  {
+    "revision": "d2a554c52189ecd065de",
+    "url": "rafacb98.github.io/proyectos/static/css/main.97ba2eeb.chunk.css"
+  },
+  {
+    "revision": "56bb782b27fe096d656c",
+    "url": "rafacb98.github.io/proyectos/static/js/2.50289b34.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "rafacb98.github.io/proyectos/static/js/2.50289b34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2a554c52189ecd065de",
+    "url": "rafacb98.github.io/proyectos/static/js/main.06b64db5.chunk.js"
+  },
+  {
+    "revision": "36f345d6cf3dbc1d9ca8",
+    "url": "rafacb98.github.io/proyectos/static/js/runtime-main.8e5c2dbe.js"
+  },
+  {
+    "revision": "030ae128f329fc7f1bf98f909cee44c0",
+    "url": "rafacb98.github.io/proyectos/static/media/bomba.030ae128.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "rafacb98.github.io/proyectos/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "rafacb98.github.io/proyectos/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "rafacb98.github.io/proyectos/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "rafacb98.github.io/proyectos/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "rafacb98.github.io/proyectos/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ proyectos/buscaminas/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "rafacb98.github.io/proyectos/precache-manifest.8ce755d4f2978365be4f5ebcf9cea921.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("rafacb98.github.io/proyectos/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
