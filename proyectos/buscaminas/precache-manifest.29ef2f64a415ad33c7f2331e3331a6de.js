8ea1829ed9805c8479fade70febc24506e9e1be7
DESKTOP-KSC88M9\Rafa <rafaelcarrillobonilla@gmail.com>
2021-07-23 12:50:54 +0200

    Soluciona error que no mostraba el proyecto del buscaminas

---
+++ proyectos/buscaminas/precache-manifest.29ef2f64a415ad33c7f2331e3331a6de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dcd4400f0ead1bc05d1194be5eb97e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1532e947a5d04b5be299",
+    "url": "./static/css/2.6a679fdf.chunk.css"
+  },
+  {
+    "revision": "3d1f20ad44ef034bcb67",
+    "url": "./static/css/main.97ba2eeb.chunk.css"
+  },
+  {
+    "revision": "1532e947a5d04b5be299",
+    "url": "./static/js/2.dfea1b8f.chunk.js"
+  },
+  {
+    "revision": "8f9a70a746088073fceab4989079aa3c",
+    "url": "./static/js/2.dfea1b8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d1f20ad44ef034bcb67",
+    "url": "./static/js/main.84b3224a.chunk.js"
+  },
+  {
+    "revision": "48d254011030f63d3fe0",
+    "url": "./static/js/runtime-main.270c405e.js"
+  },
+  {
+    "revision": "030ae128f329fc7f1bf98f909cee44c0",
+    "url": "./static/media/bomba.030ae128.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
